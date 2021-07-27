e0dd860c15a44113bf0665827acb041086e36f61
Guilherme Reis <guilhermejosereis27@gmail.com>
2021-07-27 18:35:06 -0400

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 2021_07_27_021221) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "nickname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
