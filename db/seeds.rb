4a36c8d3ae1b826d14030fdc2dee8e71ab76bd78
Guilherme Reis <guilhermejosereis27@gmail.com>
2021-07-27 18:36:35 -0400

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-# User.create!(nickname: 'Guilherme', email: 'gui@gmail.com', password: '123123', password_confirmation: '123123')
+User.create!(email: 'gui@gmail.com', password: '123123', password_confirmation: '123123')
 # Student.create!(email: 'g@gmail.com', password: '123123', password_confirmation: '123123')
