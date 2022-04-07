e140196e2d85c3f24f507b554db2bcfab5fee63a
Guilherme R <guilhermejosereis27@gmail.com>
2022-04-07 14:03:24 -0400

    Fix player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def attached_present(resource)
     resource.attached? && resource.attachment.blob.present? && resource.attachment.blob.persisted?
   end
+
+  def duration_video(video)
+    video.metadata[:duration]
+  end
 end
