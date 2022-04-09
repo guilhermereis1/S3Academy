ba3634a1d8e3a27a7d0722f224167301f4434ec8
Guilherme R <guilhermejosereis27@gmail.com>
2022-04-08 23:13:39 -0400

    Created Slider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_230311) do
+ActiveRecord::Schema.define(version: 2022_04_09_024412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -101,6 +101,13 @@ ActiveRecord::Schema.define(version: 2022_04_08_230311) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
     t.index ["course_id"], name: "index_sections_on_course_id"
+  end
+
+  create_table "sliders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.boolean "active", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "students", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
